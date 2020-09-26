8a901754d433fc947ce071d341c2a9cbd76032b4
Syed A Bilgrami <bilgrami@gmail.com>
2020-09-26 06:34:12 -0700

    initial commit

---
+++ helpaidafrica_webclient/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p><img src={logo} className="App-logo" alt="logo" style={{height: 200, width: 200}} /><br/></p>
+        <p>
+          Welcome to HelpAidAfrica Web Admin
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
